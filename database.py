4c137ffbe9bd9111a1ba9738cbe5aa1a56ec8c13
Nitin Singh Pokhariya <168846806+nitin102005@users.noreply.github.com>
2025-11-01 10:30:37 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:nitin20@localhost:5432/chatbot"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://chatbot:ju41lYCHRpuf85flhnts2AYg38ycqKiW@dpg-d42p2624d50c739un8vg-a.oregon-postgres.render.com/chatbot_n98w"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
